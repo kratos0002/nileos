0970f0d4058ec8282a3da503a7afce74e3fb6e4c
kratos0002 <vishal27792@gmail.com>
2022-02-10 14:14:33 +0530

    share code on GH

---
+++ frontend/src/data.js
@@ -0,0 +1,67 @@
+const data = {
+    products:[
+        {   _id:'1',
+            name: 'Three body problem',
+            category: 'SciFi',
+            image: 'images/p1.jpg',
+            price: 120,
+            author: 'Cixin Liu',
+            rating: 4.5,
+            numReviews:10,
+            description: 'wonderful scifi'
+        },
+        {   _id:'2',
+        name: 'Gardens of the Moon',
+        category: 'High Fantasy',
+        image: 'images/p2.jpg',
+        price: 120,
+        author: 'Cixin Liu',
+        rating: 4.5,
+        numReviews:10,
+        description: 'wonderful scifi'
+        },
+        {   _id:'3',
+        name: 'Deadhouse Gates',
+        category: 'High Fantasy',
+        image: 'images/p3.jpg',
+        price: 120,
+        author: 'Cixin Liu',
+        rating: 4.5,
+        numReviews:10,
+        description: 'wonderful scifi'
+        },
+        {   _id:'4',
+        name: 'Memories of Ice',
+        category: 'SciFi',
+        image: 'images/p4.jpg',
+        price: 120,
+        author: 'Cixin Liu',
+        rating: 4.5,
+        numReviews:10,
+        description: 'wonderful scifi'
+        },
+        {   _id:'5',
+         name: 'House of Chains',
+         category: 'SciFi',
+         image: 'images/p5.jpg',
+         price: 120,
+         author: 'Cixin Liu',
+         rating: 4.5,
+         numReviews:10,
+         description: 'wonderful scifi'
+        },
+        {   _id:'6',
+        name: 'Dark Forest',
+        category: 'SciFi',
+        image: 'images/p6.jpg',
+        price: 430,
+        author: 'Cixin Liu',
+        rating: 4.5,
+        numReviews:10,
+        description: 'wonderful scifi'
+       },
+    ]
+
+}
+
+export default data;
